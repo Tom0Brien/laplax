a119ebd04dc9f9b8b4afd3e4156160132aa7d2a5
Tom0Brien <thomas16441@gmail.com>
2025-10-22 21:25:15 +1100

    Add public API exports to package __init__.py

--- src/laplace/__init__.py
+++ src/laplace/__init__.py
@@ -1,3 +1,25 @@
-"""Laplace package."""
+"""Laplace filter for Bayesian state estimation."""
+
+from laplace.filter import LaplaceFilter, SquareRootLaplaceFilter
+from laplace.models import (
+    AnalyticLinearization,
+    GaussianMeasurementModel,
+    LinearProcessModel,
+    NonlinearProcessModel,
+    ObjectiveFunction,
+)
+from laplace.types import FilterState, NoiseSpec
 
 __version__ = "0.1.0"
+
+__all__ = [
+    "LaplaceFilter",
+    "SquareRootLaplaceFilter",
+    "FilterState",
+    "NoiseSpec",
+    "LinearProcessModel",
+    "NonlinearProcessModel",
+    "GaussianMeasurementModel",
+    "ObjectiveFunction",
+    "AnalyticLinearization",
+]
